286d9529461381b5548b0931f00cc38e3efe7ef1
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2008-02-19 20:49:55 +0000

    fairly robust now. still needs to have the album ID extracted.
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@36 658c2080-f03f-0410-a562-ea442e238c0e

--- pgms/perl/uploader.pl
+++ pgms/perl/uploader.pl
@@ -1,23 +1,27 @@
 #!/usr/bin/perl
 
-# $Id: uploader.pl,v 1.4 2006/07/12 20:11:40 swain Exp $
-
 # Basic file uploader. SW April 2006
 
 use Test::More qw(no_plan); # so it don't complain about a lack of a test plan
 use Test::WWW::Mechanize;
 use LWP::Debug qw(- debug conns trace); # turns on verbose output.
 use Data::Dumper;
-#use LWP::Debug qw(+  trace debug );
 
-die "Usage: $0 <moviefile>\n" unless $ARGV[0];
+die "Usage: $0 <moviefile> <imagefile>\n" unless $ARGV[0] && $ARGV[1];
 
 my $mech = Test::WWW::Mechanize->new();
 
 $mech->cookie_jar(HTTP::Cookies->new(file => "uploader_cookies.txt", autosave => 1));
 $mech->agent_alias( 'Linux Mozilla' ); # don't give us the java applet.
 
+print "logging in...\n";
+
 $mech->get( 'http://swain.myphotodevel.com/loginpage.php' );
+
+#printf "login page title: %s\n", $mech->title;
+$mech->title_is( 'log in to i got a lust for life' , "got the right page title for login" );
+battery_of_tests($mech);
+
 $mech->submit_form(
     form_name      => 'login_form',
 
@@ -34,37 +38,57 @@ $mech->submit_form(
 
 die "Don't appear to be logged in" unless $mech->content_like( qr/Log Out/, "No logout link found" );
 
-#my $response = $mech->follow_link( text_regex => qr/create a new album/i);
-#my $addphotoslink = extractLink($mech->content);
-#print "Link: $addphotoslink\n";
+#printf "Logged in: %s\n", $mech->title;
+$mech->title_is( 'Photo Sharing by MyPhotoAlbum.com : i got a lust for life', "logged in" );
+battery_of_tests($mech);
 
-# having logged in, call the movie upload popup directly. It's done
-# via javascript on the page, so no dice there. No soup for us.
+# having logged in, call the movie upload popup directly. Linking to
+# the movie upload page is done via javascript on the page, so no dice
+# there. No soup for us. Gotta circumvent the application and call it
+# directly.
 
+print "fetching movie upload page...\n";
 $addmovielink = "http://swain.myphotodevel.com/add_movie.php?set_albumName=album33";
 $mech->get($addmovielink);
-#$mech->get($addphotoslink);
-print $mech->content;
+
+#printf "Got movie upload form: %s\n", $mech->title;
+$mech->title_is( 'Add Video or Movie', "got the movie upload form OK");
+battery_of_tests($mech);
+
 $mech->form_name('upload_form');
 $mech->field('userfile' => $ARGV[0]);
+print "uploading movie...\n";
 my $response = $mech->submit();
 
-print "and: " . $response->header("Location");
-print Dumper($response);
+#print Dumper($response);
 
+#printf "Movie uploaded: %s\n", $mech->title;
+$mech->title_is( 'Add Video or Movie', "movie uploaded" );
+battery_of_tests($mech);
 
 # now upload una photographia
-# http://swain.myphotodevel.com/add_photos.php?set_albumName=album33
 my $addphotoslink = 'http://swain.myphotodevel.com/add_photos.php?set_albumName=album33';
+print "fetching photo upload page...\n";
 $mech->get($addphotoslink);
+
+#printf "Got photo upload form: %s\n", $mech->title;
+$mech->title_is( 'Add Photos', 'got the photo upload form' );
+battery_of_tests($mech);
+
 $mech->form_name('upload_form');
 $mech->field('userfile[]' => $ARGV[1]);
+print "uploading photo...\n";
 $response = $mech->submit();
 
-print "and: " . $response->header("Location");
-print Dumper($response);
+#printf "Photo uploaded: %s\n", $mech->title;
+$mech->title_is( 'Processing and Saving Photos', 'Photo uploaded OK' );
+battery_of_tests($mech);
 
-# parse out the album name we got, then...
+#print Dumper($response);
+
+print "end of line.\n";
+exit(0);
+
 
 
 sub extractLink {
@@ -85,4 +109,22 @@ sub dumpfileandexit {
     open OUTFILE, "> delme.html" or die $!;
     print OUTFILE $mech->content;
     exit;
+}
+
+# usage: battery_of_tests($mech);
+sub battery_of_tests {
+    my $mech = shift;
+
+
+    $mech->content_unlike( qr/fatal error/i, "No fatal errors (step $counter)");
+    $mech->content_unlike( qr/parse error/i, "No parse errors (step $counter)");
+    $mech->content_unlike( qr/mysql error/i, "No mysql errors (step $counter)");
+    $mech->content_unlike( qr/out of bounds/i, "No out of bounds errors (step $counter)");
+    $mech->content_unlike( qr/Error: No match for E-Mail Address and\/or Password\./i, "No login errors (step $counter)");
+    $mech->content_unlike( qr/SQL\/DB Error/i, "No ez_sql errors (step $counter)");
+    $mech->content_unlike( qr/Util::dump/i, "No Util::dumps (step $counter)");
+    $mech->content_unlike( qr/Warning:/i, "No PHP warnings (step $counter)");
+    $mech->content_unlike( qr/An error has occurred/i, "No 'An error has occurred' (step $counter)");
+    $mech->content_unlike( qr/Invalid username or password/i, "No invalid username/password (step $counter)");
+
 }
