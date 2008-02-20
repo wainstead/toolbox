d0092ad7f4442bdf8e5e4d4b69c432e7b959e5e7
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2008-02-20 19:50:08 +0000

    commented out my code, which didn't seem to work; pasted in an example from the intarwebs.
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@40 658c2080-f03f-0410-a562-ea442e238c0e

--- pgms/java/UrlTest.java
+++ pgms/java/UrlTest.java
@@ -1,26 +1,43 @@
-// $Id: UrlTest.java,v 1.2 2005/01/31 23:49:02 swain Exp $
+// $Id: UrlTest.java,v 1.1 2008/02/20 19:47:51 swain Exp $
 // testbed for the URL class
 
 import java.net.*;
+import java.io.*;
 import java.lang.*;
 import java.util.*;
 
 public class UrlTest {
 
     public static void main(String argv[]) {
+        /*
         try {
-        System.out.println("hello sailor!");
-        URL earl = new URL("http://www.w3.org/2001/XMLSchema");
-        java.lang.System.out.println(earl.toString());
+            System.out.println("hello sailor!");
+            URL earl = new URL("http://swain.myphotodevel.com/albums.php");
+            java.lang.System.out.println(earl.toString());
 
-        URLConnection hup = earl.openConnection();
-        System.out.println("content length: " + hup.getContentLength());
-        System.out.println("content type: " + hup.getContentType());
-        //HashMap mappy = hup.getHeaderFields();
+            URLConnection hup = earl.openConnection();
+            System.out.println("content length: " + hup.getContentLength());
+            System.out.println("content type: " + hup.getContentType());
+            //HashMap mappy = hup.getHeaderFields();
 
         } catch (Exception e) {
             System.out.println("oops, boo hoo.");
             e.printStackTrace();
         }
+        */
+        try {
+            URL yahoo = new URL("http://www.yahoo.com/");
+            URLConnection yc = yahoo.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) 
+                System.out.println(inputLine);
+            in.close();
+        } catch (Exception e) {
+            System.out.println("oops, boo hoo.");
+            e.printStackTrace();
+        }
+
     }
 }
