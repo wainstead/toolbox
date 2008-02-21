bfd108d9a0199b247eadfcbd2bb7524ff3025b60
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2008-02-21 20:31:28 +0000

    this version dies on a fatal error, for now; as I try to solve some scoping problems.
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@43 658c2080-f03f-0410-a562-ea442e238c0e

--- pgms/php/gallery.class.php
+++ pgms/php/gallery.class.php
@@ -1,7 +1,8 @@
+#!/opt/php5/bin/php
 <?php
 
-// $Id: gallery.class.php,v 1.2 2006/05/23 14:17:19 swain Exp $
-/*
+error_reporting(E_ALL);
+
 if ($argc != 2) die("usage: me <username>\n");
 $p = $argv[1];
 error_reporting(E_ERROR & ~E_WARNING);
@@ -16,76 +17,26 @@ if ( getenv('USER') != 'www' ) {
 
 ob_start();
 
-$g = new Gallery($p);
+$g = galleryFactory('swain');
+
+print "testing gallery...\n";
 $g->test();
 
 ob_flush();
 
-*/
+
 
 class Gallery {
 
     var $albumdb;
     var $username;
 
-    function Gallery($p) {
+    function __construct($p) {
 
-        global $gallery;
         $this->username = $p;
 
         ob_start();
         echo "using '$p'\n";
-
-        // FIXME: factor this out. We shoudn't be setting things in $_SERVER.
-        $_SERVER['HTTP_HOST'] = sprintf("%s.%s", $p, "service.name");
-        require("/opt/mpa/conf/gallery.mysql.conf");
-
-        //$GALLERY_BASEDIR = "/mnt/cel-1/vol01/linkless/gallery/";
-        $GALLERY_BASEDIR = "/home/swain/public_html/projects/ampiradev/gallery/";
-        $FASTSTORAGE = "/mnt/cel-1/vol01";
-
-
-        // Gallery expects to find this server variable, which is set in httpd.conf
-        $_SERVER['MPA_GALLERY_ROOT'] = $gallery->app->user_data_root;
-
-        chdir($GALLERY_BASEDIR); // fake the DocumentRoot
-
-        $_SERVER['HTTP_HOST'] = sprintf("%s.%s", $p, $gallery->app->service_name);
-
-        $GALLERY_USERDIR = 
-            "$FASTSTORAGE/$p[0]/$p[0]$p[1]/$p[0]$p[1]$p[2]/$p[0]$p[1]$p[2]$p[3]/$p[0]$p[1]$p[2]$p[3]$p[4]/$p/";
-
-        require_once($GALLERY_BASEDIR . "init.php");
-
-        require($GALLERY_USERDIR . "config.php");
-        echo "photo album url: " . $gallery->app->photoAlbumURL . "\n";
-        ob_flush();
-
-        // assert we have loaded this user's data
-        if ( ! preg_match( "/$p/", $gallery->app->albumDirURL) ) 
-            die("Failed to load the config data for '$p': " 
-                . $gallery->app->albumDirURL 
-                . "\n");
-    
-
-        // authorize us as the admin
-        $tmpUser = $gallery->userDB->getUserByUsername('admin');
-        if (! isset($tmpUser) ) {
-            stderr( "No admin user for this site.");
-            exit(3);
-        }
-
-        //stderr( "Admin loaded.\n");
-
-        $tmpUser->log("login");
-        $tmpUser->save();
-        $gallery->session->username = 'admin';
-        $gallery->user = $tmpUser;
-
-
-        $this->albumDB = new AlbumDB();
-        $gallery->session->albumName = "";
-
     }
 
 
@@ -116,4 +67,85 @@ class Gallery {
     }
 
 }
+
+/**
+ * The problem is: we instantiate $gallery in a function. That
+ * variable is not visible to incluced or required files. If it were
+ * in the global scope it would work. The alternative is to do
+ * something like:
+
+$GLOBALS['swain'] = new Gallery('swain');
+
+and everywhere we replace via regexp:
+
+$gallery->app->something
+to
+$GLOBALs['swain']->app->something
+
+Now, the problem becomes gallery.conf and config.php. Here's the remedy:
+
+1. everything in gallery.conf becomes a defined constant. Scoping solved.
+2. make crossover copies of config.php, mass regexping $gallery to $GLOBALS['sitename']
+
+Even then, there's no guarantee. But it will be fun to try.
+ */
+function galleryFactory($p) {
+    print "instantiating gallery...\n";
+    $gallery = new Gallery($p);
+    print "class check: " . get_class($gallery) . "\n";
+    // FIXME: factor this out. We shoudn't be setting things in $_SERVER.
+    $_SERVER['HTTP_HOST'] = sprintf("%s.%s", $p, "service.name");
+    require("/opt/mpa/conf/gallery.conf");
+    print "class check after loading conf: " . get_class($gallery) . "\n";
+    print "mysql host: {$gallery->app->mpa_mysql_host}}\n";
+
+    //$GALLERY_BASEDIR = "/mnt/fast/vol01/linkless/gallery/";
+    $GALLERY_BASEDIR = "/home/swain/public_html/projects/ampiradev/gallery/";
+    $FASTSTORAGE = "/mnt/fast/vol01";
+
+
+    // Gallery expects to find this server variable, which is set in httpd.conf
+    $_SERVER['MPA_GALLERY_ROOT'] = $gallery->app->user_data_root;
+
+    chdir($GALLERY_BASEDIR); // fake the DocumentRoot
+
+    $_SERVER['HTTP_HOST'] = sprintf("%s.%s", $p, $gallery->app->service_name);
+
+    $GALLERY_USERDIR = 
+        "$FASTSTORAGE/$p[0]/$p[0]$p[1]/$p[0]$p[1]$p[2]/$p[0]$p[1]$p[2]$p[3]/$p[0]$p[1]$p[2]$p[3]$p[4]/$p/";
+    print "class check before init: " . get_class($gallery) . "\n";
+    require_once($GALLERY_BASEDIR . "init.php");
+    print "class check after init: " . get_class($gallery) . "\n";
+    require($GALLERY_USERDIR . "config.php");
+    echo "photo album url: " . $gallery->app->photoAlbumURL . "\n";
+    ob_flush();
+
+    // assert we have loaded this user's data
+    if ( ! preg_match( "/$p/", $gallery->app->albumDirURL) ) 
+        die("Failed to load the config data for '$p': " 
+            . $gallery->app->albumDirURL 
+            . "\n");
+    
+
+    // authorize us as the admin
+    $tmpUser = $gallery->userDB->getUserByUsername('admin');
+    if (! isset($tmpUser) ) {
+        stderr( "No admin user for this site.");
+        exit(3);
+    }
+
+    //stderr( "Admin loaded.\n");
+
+    $tmpUser->log("login");
+    $tmpUser->save();
+    $gallery->session->username = 'admin';
+    $gallery->user = $tmpUser;
+
+
+    $gallery->albumDB = new AlbumDB();
+    $gallery->session->albumName = "";
+    print "returning gallery object: " . get_class($gallery) . "\n";
+    return $gallery;
+}
+
 ?>
