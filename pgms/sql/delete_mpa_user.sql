bc6769f9a10223846da0559a9f124cd1734f9129
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2008-03-11 14:18:53 +0000

    renamed file
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@53 658c2080-f03f-0410-a562-ea442e238c0e

--- pgms/sql/delete_mpa_user.sql
+++ pgms/sql/delete_mpa_user.sql
@@ -9,24 +9,30 @@ CREATE PROCEDURE delete_mpa_user(mpa_userid INT(10))
 MODIFIES SQL DATA
 BEGIN
 
-  DECLARE no_such_userid INT DEFAULT 0;
-  DECLARE mpauserid INT;
+DECLARE mpa_subdomain VARCHAR(30);
+DECLARE mpa_email_address VARCHAR(100);
 
-  BEGIN
-    DECLARE EXIT HANDLER FOR 1054 /* No such userid */ SET no_such_userid=1;
+SELECT username      INTO mpa_subdomain     FROM users WHERE userid = mpa_userid;
+SELECT email_address INTO mpa_email_address FROM users WHERE userid = mpa_userid;
 
-    SELECT userid INTO mpauserid FROM users WHERE userid = mpa_userid;
-    IF mpauserid > 0 THEN
-      SELECT * FROM users WHERE userid = mpa_userid;
-    END IF;
-  END;
+INSERT INTO arch_userinfo                     (SELECT * FROM userinfo                     WHERE userid = mpa_userid);
+
+INSERT INTO arch_users                        (SELECT * FROM users                        WHERE userid = mpa_userid);
+INSERT INTO arch_userstats                    (SELECT * FROM userstats                    WHERE userid = mpa_userid);
+INSERT INTO arch_mpa_club_memberships         (SELECT * FROM mpa_club_memberships         WHERE mpa_userid = mpa_userid);
+INSERT INTO arch_mpa_sites_subusers           (SELECT * FROM mpa_sites_subusers           WHERE site_id = mpa_userid);
+INSERT INTO arch_mpa_trialpay                 (SELECT * FROM mpa_trialpay                 WHERE sid = mpa_userid);
+INSERT INTO arch_mpa_mobile_upload            (SELECT * FROM mpa_mobile_upload            WHERE userid = mpa_userid);
+INSERT INTO arch_mpa_profile                  (SELECT * FROM mpa_profile                  WHERE userid = mpa_userid);
+INSERT INTO arch_photouploads                 (SELECT * FROM photouploads                 WHERE userid = mpa_userid);
+INSERT INTO arch_mpa_redeemed_features        (SELECT * FROM mpa_redeemed_features        WHERE userid = mpa_userid);
+INSERT INTO arch_mpa_users_ecards             (SELECT * FROM mpa_users_ecards             WHERE userid = mpa_userid);
+INSERT INTO arch_mpa_users_email_history      (SELECT * FROM mpa_users_email_history      WHERE parent_userid = mpa_userid);
+INSERT INTO arch_mpa_users_received_shares    (SELECT * FROM mpa_users_received_shares    WHERE received_userid = mpa_userid);
 
 
-
-IF no_such_userid = 1 THEN
-   SELECT CONCAT('Failed to remove user by userid ', mpa_userid, ': no such userid') as "Result";
-END IF;
-
+INSERT INTO arch_mpa_photos                   (SELECT * FROM mpa_photos                   WHERE subdomain = mpa_subdomain);
+INSERT INTO arch_mpa_users_address_book       (SELECT * FROM mpa_users_address_book       WHERE email_address = mpa_email_address);
 
 
 END $$
--- pgms/sql/subuser.sql
+++ pgms/sql/subuser.sql
@@ -4,20 +4,25 @@ DELIMITER $$
 
 DROP PROCEDURE IF EXISTS showuser $$
 
-CREATE PROCEDURE showuser(IN mpa_username VARCHAR(64))
+CREATE PROCEDURE showuser(IN mpa_sitename VARCHAR(64), mpa_username VARCHAR(64))
 BEGIN
 
 DECLARE siteid INT;
 DECLARE s_uid INT;
 
 -- get the users.userid, which is site_id in mpa_sites_subusers
-SELECT userid INTO siteid FROM users WHERE username = mpa_username;
+SELECT userid INTO siteid FROM users WHERE username = mpa_sitename;
 
-SELECT uid INTO s_uid FROM mpa_sites_subusers WHERE site_id = siteid;
+SELECT ss.uid
+ INTO s_uid
+ FROM mpa_subusers s, mpa_sites_subusers ss
+ WHERE ss.site_id = siteid
+ AND s.uid = ss.uid
+ AND s.username = mpa_username;
 
 SELECT * FROM mpa_sites_subusers WHERE site_id = siteid;
 
-SELECT * FROM mpa_subusers WHERE uid = s_uid;
+SELECT * FROM mpa_subusers WHERE uid = s_uid AND username = mpa_username;
 
 END $$
 
