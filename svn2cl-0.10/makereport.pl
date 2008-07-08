f5fe4af849d05d1c96edeb70016cb0be3399e99e
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2008-07-08 15:30:27 +0000

    hopefully works now
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@72 658c2080-f03f-0410-a562-ea442e238c0e

--- svn2cl-0.10/makereport.pl
+++ svn2cl-0.10/makereport.pl
@@ -1,16 +1,26 @@
 #!/usr/bin/perl
 
+#exit;
+
 use strict;
 use warnings;
 
 use Net::SMTP;
 require "/opt/mpa/lib/db-strict.pl";
 
+my $debug = 0;
+open LOG, ">> /tmp/log" or logdie $!;
+
+
+sub logdie { print LOG "$_[0]\n"; exit(1); }
+
+
 my $dbh = &MysqlConnect();
 
 # use mysql to get the dates in the format we want: 2008-07-02
 my $sth = $dbh->prepare("SELECT date_sub(date(now()), interval 1 day) AS yesterday, date(now()) AS today");
-$sth->execute;
+$sth->execute or logdie("Couldn't execute statement: $!\n");
+print LOG "\nran the sql query\n" if $debug;
 
 
 my $hash_ref = $sth->fetchrow_hashref;
@@ -18,7 +28,9 @@ my $hash_ref = $sth->fetchrow_hashref;
 $sth->finish;
 $dbh->disconnect;
 
-my $server   = 'https://svn.corp.fortunecity.com';
+logdie "No yesterday or today\n" unless ( $hash_ref->{yesterday} && $hash_ref->{today} );
+                                      
+my $server   = 'https://svn.corp.myphotoalbum.com';
 my $path     = '/home/swain/svn/svn2cl-0.10';
 my $xsltproc = '/usr/bin/xsltproc';
 my $svn      = '/opt/subversion/bin/svn';
@@ -27,7 +39,7 @@ my $svn      = '/opt/subversion/bin/svn';
 # which cannot run under 'use strict' so I whipped this up. -SW
 
 open CONFIGURATION, "< /opt/mpa/conf/mpa.conf" 
-    or die "Cannot read mpa.conf: $!\n";
+    or logdie "Cannot read mpa.conf: $!\n";
 
 my %conf = ();
 
@@ -40,25 +52,46 @@ while (my $line = <CONFIGURATION>) {
 
 my @lines = ();
 my $command = "";
+my $email_report;
 
-$command = "$svn log -r '{$hash_ref->{yesterday} 09:00:00}':'{$hash_ref->{today} 09:00:00}' $server/mpa_gallery  --xml --verbose | $xsltproc $path/svn2cl.xsl - ";
+if ($debug) {
+    $email_report = \&dont_email_report;
+} else {
+    $email_report = \&email_report;
+}
+
+$command = "$svn log -r '{$hash_ref->{yesterday} 09:00:00}':'{$hash_ref->{today} 09:00:00}' $server/mpa_gallery  --xml --verbose | $xsltproc $path/svn2cl.xsl - 2>&1";
+print LOG "$command\n" if $debug;
 @lines = `$command`;
-email_report( 'Gallery changelog', join("", @lines), $hash_ref->{yesterday} );
+print LOG "gallery lines: ", scalar(@lines), "\n";
+$email_report->( 'Gallery changelog', join("", @lines), $hash_ref->{yesterday} );
 
-$command = "$svn log -r '{$hash_ref->{yesterday} 09:00:00}':'{$hash_ref->{today} 09:00:00}' $server/mpa_products --xml --verbose | $xsltproc $path/svn2cl.xsl - ";
+$command = "$svn log -r '{$hash_ref->{yesterday} 09:00:00}':'{$hash_ref->{today} 09:00:00}' $server/mpa_products --xml --verbose | $xsltproc $path/svn2cl.xsl - 2>&1";
+print LOG "$command\n" if $debug;
 @lines = `$command`;
-email_report( 'Gifts changelog', join("", @lines), $hash_ref->{yesterday} );
+print LOG "gifts lines: ", scalar(@lines), "\n";
+$email_report->( 'Gifts changelog', join("", @lines), $hash_ref->{yesterday} );
 
-$command = "$svn log -r '{$hash_ref->{yesterday} 09:00:00}':'{$hash_ref->{today} 09:00:00}' $server/mpa_static   --xml --verbose | $xsltproc $path/svn2cl.xsl - ";
+$command = "$svn log -r '{$hash_ref->{yesterday} 09:00:00}':'{$hash_ref->{today} 09:00:00}' $server/mpa_static   --xml --verbose | $xsltproc $path/svn2cl.xsl - 2>&1";
+print LOG "$command\n" if $debug;
 @lines = `$command`;
-email_report( 'Static changelog', join("", @lines), $hash_ref->{yesterday} );
+print LOG "static lines: ", scalar(@lines), "\n";
+$email_report->( 'Static changelog', join("", @lines), $hash_ref->{yesterday} );
 
-$command = "$svn log -r '{$hash_ref->{yesterday} 09:00:00}':'{$hash_ref->{today} 09:00:00}' $server/mpa_cart     --xml --verbose | $xsltproc $path/svn2cl.xsl - ";
+$command = "$svn log -r '{$hash_ref->{yesterday} 09:00:00}':'{$hash_ref->{today} 09:00:00}' $server/mpa_cart     --xml --verbose | $xsltproc $path/svn2cl.xsl - 2>&1";
+print LOG "$command\n" if $debug;
 @lines = `$command`;
-email_report( 'Cart changelog', join("", @lines), $hash_ref->{yesterday} );
+print LOG "cart lines: ", scalar(@lines), "\n";
+$email_report->( 'Cart changelog', join("", @lines), $hash_ref->{yesterday} );
+
+
+close LOG;
 
 
 
+sub dont_email_report {
+    for my $line (@_) { print LOG "line: $line"; }
+}
 
 sub email_report {
 
@@ -68,14 +101,14 @@ sub email_report {
     my $smtp = Net::SMTP->new($conf{'smtpserver'}, Debug => 0);
 
     unless ($smtp) {
-        die "$0: ERROR: Could not connect to the mail server: $!\n";
+        logdie "$0: ERROR: Could not connect to the mail server: $!\n";
     } 
 
     eval {
         #print("Attempting to send the message...");
         $smtp->mail($conf{sender});
         $smtp->to('swain@myphotoalbum.com');
-        #$smtp->cc('fmarte@myphotoalbum.com');
+        $smtp->cc('fmarte@myphotoalbum.com', 'jessy@myphotoalbum.com');
 
         $smtp->data();
 
