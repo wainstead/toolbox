11b9cbe059eab96a278af778e526e0e324b1a5e5
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2008-07-18 14:14:13 +0000

    added comment.
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@78 658c2080-f03f-0410-a562-ea442e238c0e

--- pgms/sql/expire.sql
+++ pgms/sql/expire.sql
@@ -1,12 +1,14 @@
--- set prem_packages_history.expire_date for the users expire2, expire15, expire30 and expired.
+-- set mpa_club_memberships.expire_date for the users expire2, expire15, expire30 and expired.
 -- 7/3/7: added expire3years
 -- 12/19/07: added update to last_purchase_date
+-- 7/18/08: updated to use the mpa_club_memberships table.
+
 update users set last_purchase_date= now() where username in  ('expire2','expire15','expire30','expired');
 
-update prem_packages_history set expire_date = date_add(date(now()), interval 14 day) where customer_id = (select userid from users where username='expire15');
-update prem_packages_history set expire_date = date_add(date(now()), interval 1 day) where customer_id = (select userid from users where username='expire2');
-update prem_packages_history set expire_date = date_add(date(now()), interval 29 day) where customer_id = (select userid from users where username='expire30');
-update prem_packages_history set expire_date = date_sub(date(now()), interval 55 day) where customer_id=(select userid from users where username='expired');
-update prem_packages_history set expire_date = date_add(date(now()), interval 3 year) where customer_id = (select userid from users where username='expire3years');
+update mpa_club_memberships set expire_date = date_add(date(now()), interval 14 day) where mpa_userid = (select userid from users where username='expire15');
+update mpa_club_memberships set expire_date = date_add(date(now()), interval 1 day)  where mpa_userid = (select userid from users where username='expire2');
+update mpa_club_memberships set expire_date = date_add(date(now()), interval 29 day) where mpa_userid = (select userid from users where username='expire30');
+update mpa_club_memberships set expire_date = date_sub(date(now()), interval 55 day) where mpa_userid = (select userid from users where username='expired');
+update mpa_club_memberships set expire_date = date_add(date(now()), interval 3 year) where mpa_userid = (select userid from users where username='expire3years');
 
-select username, expire_date, userid, to_days(expire_date) - to_days(date(now())) as d  from users, prem_packages_history where userid=customer_id and username like 'expire%';
+select username, expire_date, userid, to_days(expire_date) - to_days(date(now())) as d  from users, mpa_club_memberships where userid=mpa_userid and username like 'expire%';
