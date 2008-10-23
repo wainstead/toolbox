36a866e32c1752d8dd9570151594bf8713cc2bcd
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2008-10-23 22:14:31 +0000

    added comments
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@93 658c2080-f03f-0410-a562-ea442e238c0e

--- generate_svn.pl
+++ generate_svn.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+# copies trunk to tags/STAGING for all projects, using Perl's magic `` HERE doc notation.
+# Probably not flexible enough for the long haul but enough to satisfy my laziness for now.
+
 @projects = qw(gallery cart products);
 
 for (@projects) {
