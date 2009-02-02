6fce8ccb170a5047cf6ca07a8d65eec523966e9f
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2009-02-02 15:19:12 +0000

    rename userid to id
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@108 658c2080-f03f-0410-a562-ea442e238c0e

--- usertable.sql
+++ usertable.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `swain_test`;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `swain_test` (
-  `userid` int(10) unsigned NOT NULL default '0',
+  `id` int(10) unsigned NOT NULL default '0',
   `username` varchar(30) NOT NULL default '',
   `password` varbinary(32) NOT NULL default '',
   `email_address` varchar(100) NOT NULL default '',
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-02-02 15:16:52
+-- Dump completed on 2009-02-02 15:19:02
