f85d07c1d75ff4eec3e3cc1f49deaa6111996ff4
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2009-03-06 16:53:13 +0000

    cleaned up version
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@139 658c2080-f03f-0410-a562-ea442e238c0e

--- pgms/php/newpathmaker.php
+++ pgms/php/newpathmaker.php
@@ -1,21 +1,22 @@
 #!/opt/php5/bin/php
 <?php
-
+require_once('/opt/mpa/conf/gallery.conf');
   // /d0/tree/000/143/000143377-coretest
 
 $userid   = "143377";
 $username = "coretest";
 
-getAlbumPath($userid);
+print getAlbumPathTopLevel($userid, $username) . "\n";
 
-function getAlbumPath($userid) {
+function getAlbumPathTopLevel($userid, $username) {
+    // pad the userid with leading zeroes
     $padded_uid = sprintf("%09d", $userid);
-    $threesome  = substr($padded_uid, 0, 3);
-    $sixsome    = substr($padded_uid, 3, 3);
-    print "padded: $padded_uid\n";
-    print "first three: $threesome\n";
-    print "second: $sixsome\n";
-    print "sized monty: /d0/tree/$threesome/$sixsome/{$padded_uid}-{$username}\n";
-    print "full monty: /d0/tree/$threesome/$sixsome/{$padded_uid}-{$username}-B\n";
+
+    return sprintf("%s/%s/%s/{$padded_uid}-{$username}",
+                   MPA_USERS_ALBUMS_ROOT,       // i.e. /d0/tree
+                   substr($padded_uid, 0, 3),   // i.e. 000
+                   substr($padded_uid, 3, 3)    // i.e. 143
+                   );
 }
+
 ?>
