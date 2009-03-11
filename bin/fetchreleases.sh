c23c878d83a95120362c0ff35de771f3e9ab1eaf
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2009-03-11 18:35:07 +0000

    spiffy awk bit provided by bill
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@147 658c2080-f03f-0410-a562-ea442e238c0e

--- bin/fetchreleases.sh
+++ bin/fetchreleases.sh
@@ -1,8 +1,8 @@
 [ -e /opt/subversion/bin/svn ] && export PATH=/opt/subversion/bin:$PATH
 
-[ -d /opt/mpa_cart/current     ] && cd /opt/mpa_cart/current     && echo -n "cart:     " && svn info | grep URL
-[ -d /opt/mpa_core/current     ] && cd /opt/mpa_core/current     && echo -n "core:     " && svn info | grep URL
-[ -d /opt/mpa_gallery/current  ] && cd /opt/mpa_gallery/current  && echo -n "gallery:  " && svn info | grep URL
-[ -d /opt/mpa_products/current ] && cd /opt/mpa_products/current && echo -n "products: " && svn info | grep URL
-[ -d /opt/mpa                  ] && cd /opt/mpa                  && echo -n "signup:   " && svn info | grep URL
-[ -d /opt/mpa_www              ] && cd /opt/mpa_www              && echo -n "www:      " && svn info | grep URL
+[ -d /opt/mpa_cart/current     ] && cd /opt/mpa_cart/current     && echo -n "cart:     " && svn info | grep URL | awk -F/ '{print $NF}'
+[ -d /opt/mpa_core/current     ] && cd /opt/mpa_core/current     && echo -n "core:     " && svn info | grep URL | awk -F/ '{print $NF}'
+[ -d /opt/mpa_gallery/current  ] && cd /opt/mpa_gallery/current  && echo -n "gallery:  " && svn info | grep URL | awk -F/ '{print $NF}'
+[ -d /opt/mpa_products/current ] && cd /opt/mpa_products/current && echo -n "products: " && svn info | grep URL | awk -F/ '{print $NF}'
+[ -d /opt/mpa                  ] && cd /opt/mpa                  && echo -n "signup:   " && svn info | grep URL | awk -F/ '{print $NF}'
+[ -d /opt/mpa_www              ] && cd /opt/mpa_www              && echo -n "www:      " && svn info | grep URL | awk -F/ '{print $NF}'
