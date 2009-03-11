5e17ae45c616f35ceab59a995b89ce2fedfa67d6
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2009-03-11 18:02:27 +0000

    find the fachunking release tags
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@144 658c2080-f03f-0410-a562-ea442e238c0e

---
+++ bin/fetchreleases.sh
@@ -0,0 +1,6 @@
+[ -d /opt/mpa_cart/current     ] && cd /opt/mpa_cart/current     && echo -n "cart:     " && svn info | grep URL
+[ -d /opt/mpa_core/current     ] && cd /opt/mpa_core/current     && echo -n "core:     " && svn info | grep URL
+[ -d /opt/mpa_gallery/current  ] && cd /opt/mpa_gallery/current  && echo -n "gallery:  " && svn info | grep URL
+[ -d /opt/mpa_products/current ] && cd /opt/mpa_products/current && echo -n "products: " && svn info | grep URL
+[ -d /opt/mpa_signup/current   ] && cd /opt/mpa_signup/current   && echo -n "signup:   " && svn info | grep URL
+[ -d /opt/mpa_www/current      ] && cd /opt/mpa_www/current      && echo -n "www:      " && svn info | grep URL
