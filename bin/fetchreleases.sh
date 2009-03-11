05a6d0266f7aeb94ee28bf33ea47a7801b7e7e63
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2009-03-11 18:10:35 +0000

    Modify the PATH if there's an opt/subversion/bin
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@145 658c2080-f03f-0410-a562-ea442e238c0e

--- bin/fetchreleases.sh
+++ bin/fetchreleases.sh
@@ -1,3 +1,5 @@
+[ -e /opt/subversion/bin/svn ] && export PATH=/opt/subversion/bin:$PATH
+
 [ -d /opt/mpa_cart/current     ] && cd /opt/mpa_cart/current     && echo -n "cart:     " && svn info | grep URL
 [ -d /opt/mpa_core/current     ] && cd /opt/mpa_core/current     && echo -n "core:     " && svn info | grep URL
 [ -d /opt/mpa_gallery/current  ] && cd /opt/mpa_gallery/current  && echo -n "gallery:  " && svn info | grep URL
