1ef80d7b87c461d1dc823ad9058f0f02f116c209
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2009-03-11 18:25:10 +0000

    set the bin path to find svn
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@146 658c2080-f03f-0410-a562-ea442e238c0e

--- bin/fetchreleases.sh
+++ bin/fetchreleases.sh
@@ -4,5 +4,5 @@
 [ -d /opt/mpa_core/current     ] && cd /opt/mpa_core/current     && echo -n "core:     " && svn info | grep URL
 [ -d /opt/mpa_gallery/current  ] && cd /opt/mpa_gallery/current  && echo -n "gallery:  " && svn info | grep URL
 [ -d /opt/mpa_products/current ] && cd /opt/mpa_products/current && echo -n "products: " && svn info | grep URL
-[ -d /opt/mpa_signup/current   ] && cd /opt/mpa_signup/current   && echo -n "signup:   " && svn info | grep URL
-[ -d /opt/mpa_www/current      ] && cd /opt/mpa_www/current      && echo -n "www:      " && svn info | grep URL
+[ -d /opt/mpa                  ] && cd /opt/mpa                  && echo -n "signup:   " && svn info | grep URL
+[ -d /opt/mpa_www              ] && cd /opt/mpa_www              && echo -n "www:      " && svn info | grep URL
