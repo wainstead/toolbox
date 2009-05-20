6b6c3a48774f5231f33b29e2d52d5806c4def1d4
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2009-05-20 14:34:45 +0000

    don't use extended inserts. makes diffing useless.
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@223 658c2080-f03f-0410-a562-ea442e238c0e

--- dumpproducts.sh
+++ dumpproducts.sh
@@ -5,21 +5,22 @@ if [ $# -ne 1 ]; then
         exit
 fi
 
+MYSQLDUMP='/d0/mysql/mpa/bin/mysqldump --extended-insert=false -S /tmp/mysql.mpa.sock -uroot mpa'
+TARGETDIR=/home/swain/public_html/projects/schema
 
 # dump the critical product/category tables out.
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa mpa_categories                 | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/mpa_categories.sql                  
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa ofs_categories_to_prefix_codes | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/ofs_categories_to_prefix_codes.sql  
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa osc_categories                 | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/osc_categories.sql                  
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa osc_categories_description     | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/osc_categories_description.sql      
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa osc_products_to_categories     | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/osc_products_to_categories.sql      
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa mediaclip_products_properties  | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/mediaclip_products_properties.sql   
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa mpa_products_groupings         | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/mpa_products_groupings.sql          
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa osc_products                   | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/osc_products.sql                    
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa osc_products_attributes        | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/osc_products_attributes.sql         
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa osc_products_classes           | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/osc_products_classes.sql            
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa osc_products_description       | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/osc_products_description.sql        
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa osc_products_properties        | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/osc_products_properties.sql         
-/d0/mysql/mpa/bin/mysqldump -S /tmp/mysql.mpa.sock -uroot mpa osc_products_to_categories     | grep -v 'Dump completed on'   > /home/swain/public_html/projects/schema/products-$1/osc_products_to_categories.sql      
+$MYSQLDUMP mpa_categories                 | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/mpa_categories.sql                  
+$MYSQLDUMP ofs_categories_to_prefix_codes | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/ofs_categories_to_prefix_codes.sql  
+$MYSQLDUMP osc_categories                 | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/osc_categories.sql                  
+$MYSQLDUMP osc_categories_description     | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/osc_categories_description.sql      
+$MYSQLDUMP osc_products_to_categories     | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/osc_products_to_categories.sql      
+$MYSQLDUMP mediaclip_products_properties  | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/mediaclip_products_properties.sql   
+$MYSQLDUMP mpa_products_groupings         | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/mpa_products_groupings.sql          
+$MYSQLDUMP osc_products                   | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/osc_products.sql                    
+$MYSQLDUMP osc_products_classes           | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/osc_products_classes.sql            
+$MYSQLDUMP osc_products_description       | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/osc_products_description.sql        
+$MYSQLDUMP osc_products_properties        | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/osc_products_properties.sql         
+$MYSQLDUMP osc_products_to_categories     | grep -v 'Dump completed on'   > $TARGETDIR/products-$1/osc_products_to_categories.sql      
 
 cd /home/swain/public_html/projects/schema/products-$1/
 /opt/subversion/bin/svn commit -m "updated product tables"
