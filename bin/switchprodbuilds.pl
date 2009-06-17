db57f3eea3ff43b9cee2dae116325fd46dc837c8
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2009-06-17 20:45:47 +0000

    cleaning up before i check out again from the newer shorter domain name
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@252 658c2080-f03f-0410-a562-ea442e238c0e

--- bin/switchprodbuilds.pl
+++ bin/switchprodbuilds.pl
@@ -4,13 +4,11 @@ while ($line = <DATA>) {
     chomp $line;
     ($project, $tag) = split /: +/, $line;
     next if $tag eq 'trunk';
-    print "cd /home/swain/public_html/projects/mpa_${project}_prod; svn switch https://svn.corp.myphotoalbum.com/mpa_$project/tags/$tag\n";
+    print "cd /home/swain/public_html/projects/mpa_${project}_prod; svn switch https://svn.corp.myphotoalbum.com/mpa_$project/tags/$tag; ";
 }
-
+print "\n";
 __END__
-cart:     R_2009_05_13_mass_cleanup_deux
-core:     R_2009_05_13_new_header_footer
-gallery:  R_2009_05_13_this_years_redesign
-products: R_2009_05_13_gifterator
-signup:   trunk
-www:      trunk
+cart:     R_2009_05_26_bugfixes_galore
+core:     R_2009_06_05_not_for_sale_fix
+gallery:  R_2009_06_05_search_security_fix
+products: R_2009_06_05_multi_photo_upload_gmc
