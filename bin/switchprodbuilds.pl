2956ec62890876d3440571f6e1f32cd45961c68f
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2009-11-04 20:20:46 +0000

    for lint checking the patches I make for Jessy's mass commits
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@371 658c2080-f03f-0410-a562-ea442e238c0e

--- bin/switchprodbuilds.pl
+++ bin/switchprodbuilds.pl
@@ -8,7 +8,7 @@ while ($line = <DATA>) {
 }
 print "\n";
 __END__
-mpa_core: R_2009_08_04_website_classes
-mpa_gallery: R_2009_08_04_yellowbox
-mpa_cart: R_2009_08_04_new_signup_path
-mpa_products: R_2009_08_04_deal_of_the_day
+mpa_core: R_2009_11_04_terms_and_analytics
+mpa_gallery: R_2009_11_04_plr_video
+mpa_cart: R_2009_11_04_support_link_update
+mpa_products: R_2009_11_04_typo_lesbo_strikes_again
