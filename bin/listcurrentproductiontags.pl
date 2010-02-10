a014d85ffe5c048094d50bbe5619722913c63e46
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2010-02-10 17:32:07 +0000

    bringing stuff into 2010
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@466 658c2080-f03f-0410-a562-ea442e238c0e

--- bin/listcurrentproductiontags.pl
+++ bin/listcurrentproductiontags.pl
@@ -5,7 +5,7 @@
 foreach my $project ('mpa_core', 'mpa_gallery', 'mpa_cart', 'mpa_products') {
     my $result = '';
     
-    foreach my $line (`svn ls https://svn.myphotoalbum.com/$project/tags  | grep R_2009 | sort`) {
+    foreach my $line (`svn ls https://svn.myphotoalbum.com/$project/tags  | grep R_2010 | sort`) {
         $result =  $line;
     }
     chomp $result;
