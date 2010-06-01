578c6a779f4d58e926259ff4694e6aad2c92824e
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2010-06-01 20:22:26 +0000

    upload all my uploadable-sized photos, naming the albums after the folders on my Mac.
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@566 658c2080-f03f-0410-a562-ea442e238c0e

--- pgms/py/upload.py
+++ pgms/py/upload.py
@@ -10,16 +10,23 @@ dirList = os.listdir(path)
 
 gallery = gallery_remote2.Gallery('http://swainfree.myphotodevel.com')
 
+folderPattern = re.compile('^uploadable')
+isImage = re.compile('jpg$', re.IGNORECASE)
+
 for fname in dirList:
-    pattern = re.compile('^uploadable')
-    if pattern.match(fname):
+
+    if folderPattern.match(fname):
         print fname
         gallery.login('admin', '112233')
         albumname = gallery.newAlbum(0, None, fname)
         photoDir = "%(p)s/%(f)s" % { 'p': path, 'f': fname }
         fileList = os.listdir(photoDir)
+        x = 0
         for iname in fileList:
+            if not isImage.search(iname): continue
             photoFile = "%(p)s/%(f)s/%(i)s" % { 'p': path, 'f': fname, 'i' : iname }
             print photoFile
             gallery.login('admin', '112233')
             gallery.addItem(albumname, photoFile, iname)
+            x += 1
+            if x > 5: break
