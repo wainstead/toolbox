104708c5f2a5d5360b19ac6f0cc47709ca33c797
swain <swain@658c2080-f03f-0410-a562-ea442e238c0e>
2010-09-15 18:42:53 +0000

    list www and signup tags too.
    git-svn-id: https://svn.myphotoalbum.com/swain/trunk@641 658c2080-f03f-0410-a562-ea442e238c0e

--- bin/listcurrentproductiontags.pl
+++ bin/listcurrentproductiontags.pl
@@ -2,7 +2,7 @@
 
 
 
-foreach my $project ('mpa_core', 'mpa_gallery', 'mpa_cart', 'mpa_products') {
+foreach my $project ('mpa_signup', 'mpa_www', 'mpa_core', 'mpa_gallery', 'mpa_cart', 'mpa_products') {
     my $result = '';
     
     foreach my $line (`svn ls https://svn.myphotoalbum.com/$project/tags  | grep R_2010 | sort`) {
