bb140e4496215c5a08f803937153bc06391c5b8a
Steve Wainstead <wainstead@gmail.com>
2012-05-25 17:34:03 -0700

    Solution to homework 3-2

---
+++ pgms/py/udacity/homework3/reading_machine_minds.py
@@ -0,0 +1,139 @@
+# Reading Machine Minds 2
+#
+# We say that a finite state machine is "empty" if it accepts no strings.
+# Similarly, we say that a context-free grammar is "empty" if it accepts no
+# strings. In this problem, you will write a Python procedure to determine
+# if a context-free grammar is empty.
+#
+# A context-free grammar is "empty" starting from a non-terminal symbol S 
+# if there is no _finite_ sequence of rewrites starting from S that
+# yield a sequence of terminals. 
+#
+# For example, the following grammar is empty:
+#
+# grammar1 = [ 
+#       ("S", [ "P", "a" ] ),           # S -> P a
+#       ("P", [ "S" ]) ,                # P -> S
+#       ] 
+#       
+# Because although you can write S -> P a -> S a -> P a a -> ... that
+# process never stops: there are no finite strings in the language of that
+# grammar. 
+#
+# By contrast, this grammar is not empty: 
+#
+# grammar2 = [
+#       ("S", ["P", "a" ]),             # S -> P a
+#       ("S", ["Q", "b" ]),             # S -> Q b
+#       ("P", ["P"]),                   # P -> P
+#       ("Q", ["c", "d"]),              # Q -> c d 
+#
+# And ["c","d","b"] is a witness that demonstrates that it accepts a
+# string.
+#
+# Write a procedure cfgempty(grammar,symbol,visited) that takes as input a
+# grammar (encoded in Python) and a start symbol (a string). If the grammar
+# is empty, it must return None (not the string "None", the value None). If
+# the grammar is not empty, it must return a list of terminals
+# corresponding to a string in the language of the grammar. (There may be
+# many such strings: you can return any one you like.) 
+#
+# To avoid infinite loops, you should use the argument 'visited' (a list)
+# to keep track of non-terminals you have already explored. 
+#
+# Hint 1: Conceptually, in grammar2 above, starting at S is not-empty with
+# witness [X,"a"] if P is non-empty with witness X and is non-empty with
+# witness [Y,"b"] if Q is non-empty with witness Y. 
+#
+# Hint 2: Recursion! A reasonable base case is that if your current
+# symbol is a terminal (i.e., has no rewrite rules in the grammar), then
+# it is non-empty with itself as a witness. 
+#
+# Hint 3: all([True,False,True]) = False
+#         any([True,True,False]) = True
+
+def is_symbol(grammar, candidate):
+    for rule in grammar:
+        if candidate == rule[0]:
+            return True
+    return False
+
+def is_terminal(grammar, token):
+    return not is_symbol(grammar, token)
+
+# returns: a list of terminals, i.e. ['c', 'd', 'b']
+def cfgempty(grammar, symbol, visited):
+
+    #print "received %s" % symbol
+    #print "visited: %s" % visited
+
+    if symbol in visited:
+        #print "%s was visited, returning None" % symbol
+        return None
+
+    if is_terminal(grammar, symbol):
+        return [symbol,]
+
+    for rule in grammar:
+        if symbol == rule[0]:
+            #print "--> Doing rule %s for symbol %s" % (rule, symbol)
+            exp = rule[1]
+            mylist = list()
+            if exp == []:
+                # Epsilon
+                return mylist
+
+            for token in exp:
+                #print "doing token %s in exp %s" % (token, exp)
+                result = cfgempty(grammar, token, visited + [symbol])
+                if result == []:
+                    #print "Epsilon"
+                    pass
+                elif type(result) == type(list()):
+                    mylist.extend(result)
+                else:
+                    mylist.append(result)
+                #print "mylist now %s" % mylist
+                if result is None:
+                    break
+            if all(mylist):
+                return mylist
+
+    if not all(mylist):
+        #print "Returning None for: %s" % mylist
+        return None
+    #print "returning %s" % mylist
+    return mylist
+                
+# We have provided a few test cases for you. You will likely want to add
+# more of your own. 
+
+grammar1 = [ 
+      ("S", [ "P", "a" ] ),           
+      ("P", [ "S" ]) ,               
+      ] 
+                        
+print cfgempty(grammar1,"S",[]) == None 
+
+grammar2 = [
+      ("S", ["P", "a" ]),             
+      ("S", ["Q", "b" ]),             
+      ("P", ["P"]), 
+      ("Q", ["c", "d"]),              
+      ] 
+
+print cfgempty(grammar2,"S",[]) == ['c', 'd', 'b']
+
+
+grammar3 = [  # some Spanish provinces
+        ("S", [ "Barcelona", "P", "Huelva"]),
+        ("S", [ "Q" ]),
+        ("Q", [ "S" ]),
+        ("P", [ "Las Palmas", "R", "Madrid"]),
+        ("P", [ "T" ]),
+        ("T", [ "T", "Toledo" ]),
+        ("R", [ ]) ,
+        ("R", [ "R"]), 
+        ]
+
+print cfgempty(grammar3,"S",[]) == ['Barcelona', 'Las Palmas', 'Madrid', 'Huelva']
