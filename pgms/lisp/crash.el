4cbf52106021525903b4793e624858eea80a0025
Steve Wainstead <swain@socialserve.com>
2013-07-26 17:33:51 -0700

    Lisp code that crashes Emacs 24.3 on OS X Lion.

---
+++ pgms/lisp/crash.el
@@ -0,0 +1,38 @@
+(defun set-background-color (color-name)
+  "Set the background color of the selected frame to COLOR-NAME.
+When called interactively, prompt for the name of the color to use.
+To get the frame's current background color, use `frame-parameters'."
+  (interactive (list (read-color "Background color: ")))
+  (shell-command (format "/bin/echo 'Modifying frame parameters' >> /Users/swain/.emacs.shellbuffers/colorchanges\n"))
+    (modify-frame-parameters (selected-frame)
+			   (list (cons 'background-color color-name)))
+  (shell-command (format "/bin/echo 'Setting face...' >> /Users/swain/.emacs.shellbuffers/colorchanges\n"))
+  (or window-system
+      (face-set-after-frame-default (selected-frame)))
+  (shell-command (format "/bin/echo 'done.' >> /Users/swain/.emacs.shellbuffers/colorchanges\n"))
+)
+
+(random t)
+;; choose random colors every time we compile, just for fun
+(defun sw-make-random-hex-color-string ()
+  "Return a string in the form of #FFFFFF. Choose the number for
+   #xffffff randomly using Emacs Lisp's builtin function (random)."
+  ;; seed our random number generator: current datetime plus Emacs's
+  ;; process ID
+
+  ;; this probably isn't the problem because: it never trips up on
+  ;; set-foreground-color and it would, wouldn't it?
+  (format "#%06x" (random #xffffff))
+  )
+
+
+(setq flag t)
+(setq counter 0)
+(while flag
+    (sleep-for 0.25)
+    (setq bg-color (sw-make-random-hex-color-string))
+    (set-background-color bg-color)
+    (message (format "%s: loops" counter))
+    (setq counter (+ counter 1))
+    ;;(if (> counter 10) (setq flag nil))
+)
