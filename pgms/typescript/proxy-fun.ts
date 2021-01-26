6e42e1be4e2e8d646af97dc67da621c66429d436
Steve Wainstead <spawlowski@diligent.com>
2021-01-26 13:56:35 -0500

    Cleaned up/simplified version of GET with axios

--- pgms/typescript/proxy-fun.ts
+++ pgms/typescript/proxy-fun.ts
@@ -1,29 +1,12 @@
-import { isConstructorDeclaration } from 'typescript'
-
-export {}
 const axios = require('axios')
 
-// console.log('Hello!')
-// console.log(`Hello! ${process.env.FOO}`)
-
-const callit = async () => {
-  const res = await axios.get('https://diligent.com/', {
+axios
+  .get('https://diligent.com/', {
     proxy: {
       host: process.env.DIL_PROXY_HOST,
       port: 3128
     }
   })
-  return res
-}
-// .then() {
-//     console.log(res.data)
-// }
-// .catch (e) {
-//     console.log(e)
-// }
-
-//console.log(res)
-callit()
   .then((res) => {
     console.log(res.status)
   })
