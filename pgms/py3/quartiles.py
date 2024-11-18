3ab086c94f8e81b1c769ed47b667ab743bdc30c5
Steve Wainstead <swain@panix.com>
2024-11-18 16:17:47 -0500

    Added info/context for future hacking

--- pgms/py3/quartiles.py
+++ pgms/py3/quartiles.py
@@ -1,6 +1,17 @@
 """
+
+First, make a screenshot of the Quartiles game. Rename it so there's
+no spaces in the file name (it will be "Screen shot somedate sometime
+.ext" or somesuch). Then put the path to that file into the script
+below. With Copilot I'd make it rewrite the script to take a command
+line argument for the file.
+
+
+$ export PATH=/opt/homebrew/bin:$PATH # for both python3 from Homebrew, and the 'tesseract' binary
+$ which python3
+$ python3 -m venv env
 $ source env/bin/activate
-$ export PATH=$PATH:/opt/homebrew/bin # for the 'tesseract' binary
+$ pip install pytesseract
 $ python quartiles.py
 
 """
@@ -26,4 +37,34 @@ def extract_text_from_image(image_path):
 image_path = '/Users/swain/Desktop/quartiles-11-16-2024.png'
 extracted_text = extract_text_from_image(image_path)
 print(extracted_text)
+
+
+
+"""
+So that works... next Gemini proposes a brute force approach to
+finding the quartiles:
+
+
+import itertools
+
+def is_valid_word(word):
+    # Implement your word validity check here
+    # You can use a dictionary or a language model
+    return word in your_word_list
+
+def find_quartiles(words):
+    for combination in itertools.combinations(words, 4):
+        remaining_words = [word for word in words if word not in combination]
+        for second_combination in itertools.combinations(remaining_words, 4):
+            # ... and so on, for the remaining 3 combinations
+
+            # Check if all 5 combinations are valid words
+            if all(is_valid_word(group) for group in [combination, second_combination, ...]):
+                print(combination, second_combination, ...)
+
+# Example usage:
+words = ['beh', 'rus', 'the', 'less', 'ce', 'nds', 'go', 'er', 'rifr', 'av', 'sau', 'ste', 'nev']
+find_quartiles(words)
+
+"""
 
